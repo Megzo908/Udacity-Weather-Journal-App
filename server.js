8bf08985e109217985ee5515defb2769aaeaedef
Omar Magdy <omarmagdy151299@gmail.com>
2022-08-19 13:24:47 +0200

    Server and App setup using the weather API

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
+// Body Parser
+const bodyParser = require('body-parser')
+const port = 3000
+
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {}
 
-// Require Express to run server and routes
-
-// Start up an instance of app
+// Require Express to run server and routes and start an instance of app
+const express = require('express')
+const app = express()
+// Initialize the main project folder
+app.use(express.static('website'))
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
-// Initialize the main project folder
-app.use(express.static('website'));
-
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
 
 // Setup Server
+app.get('/send', (req, res) => {
+  res.send(projectData)
+})
+
+app.post('/add', (req, res) => {
+  projectData = {
+    temp: req.body.data.main.temp,
+    date: req.body.newDate,
+    content: req.body.feelings,
+  }
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,48 @@
-/* Global Variables */
+// Global Variables
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const API_KEY = '&appid=a367587e8fdf0b056f1008204dea2b80&units=metric'
+const SERVER_URL = 'http://localhost:3000/'
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let d = new Date()
+let newDate = d.toDateString()
+
+// HTML Selectors
+const generate = document.querySelector('#generate')
+const entryholder = document.querySelector('#entryHolder')
+const date = document.querySelector('#date')
+const temp = document.querySelector('#temp')
+let content = document.querySelector('#content')
+
+// Getting data from API
+const APIdata = async () => {
+  let zip = document.querySelector('#zip').value
+  let feelings = document.querySelector('#feelings').value
+  // API
+  const data = await (await fetch(BASE_URL + zip + API_KEY)).json()
+  // Update UI with API data
+  date.innerText = newDate
+  temp.innerText = data.main.temp + '°C'
+  content.innerText = feelings
+  // Send and then receive data from the server
+  sendToServer(`${SERVER_URL}add`, { data, newDate, feelings })
+  receiveFromServer()
+}
+// Send data to the server with fetch post method
+const sendToServer = async (url = '', data = {}) => {
+  const sentData = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+}
+// Update UI with server data
+const receiveFromServer = async () => {
+  const res = await (await fetch(`${SERVER_URL}send`)).json() // Default method on fetch **GET
+  date.innerText = res.date
+  temp.innerText = res.temp + '°C'
+  content.innerText = res.content
+}
+
+// Update UI when button is clicked
+generate.addEventListener('click', APIdata)
